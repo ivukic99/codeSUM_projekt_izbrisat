94774b8304f3c85f1100edd57b4ce4f5733beea9
Stanko Bebek <stanko.bebek82@gmail.com>
2020-12-20 22:00:56 +0100

    added roles in user api

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,9 +4,9 @@ import Router from 'vue-router';
 import Landing from '../views/Landing.vue';
 import Auth from '../views/auth/Auth.vue';
 import ResetPassword from '../views/auth/ResetPassword.vue';
-import Courses from '../views/Courses.vue';
 import BecomeMember from '../views/BecomeMember.vue';
-import Home from '../views/Home.vue';
+import Courses from '../views/Courses.vue';
+import Home from '../views/app/Home.vue';
 import Stanko from '../views/creators/Stanko.vue';
 import Igor from '../views/creators/Igor.vue';
 
--- resources/js/views/app/Home.vue
+++ resources/js/views/app/Home.vue
@@ -10,7 +10,7 @@
 
 <script>
 import { mapGetters } from 'vuex';
-import NavbarUser from '../components/NavbarUser.vue';
+import NavbarUser from '../../components/NavbarUser.vue';
 export default {
     components:{
         NavbarUser
