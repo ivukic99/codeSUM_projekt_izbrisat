ac710997f19036189470833a5d454405e51266b7
Igor Vukic <igor.vukic8877@gmail.com>
2020-12-22 12:43:08 +0100

    reset password

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,11 +3,10 @@ import Router from 'vue-router';
 
 import Landing from '../views/Landing.vue';
 import Auth from '../views/auth/Auth.vue';
-import ResetPassword from '../views/auth/ResetPassword.vue';
 import Forgot from '../views/auth/Forgot.vue';
 import Reset from '../views/auth/Reset.vue';
-import BecomeMember from '../views/BecomeMember.vue';
 import Courses from '../views/Courses.vue';
+import BecomeMember from '../views/BecomeMember.vue';
 import Courses from '../views/Courses.vue';
 import CoursesUser from '../views/app/CoursesUser.vue';
 import Profile from '../views/app/Profile.vue';
@@ -48,14 +47,6 @@ const routes = [
         path: '/auth',
         name: 'Auth',
         component: Auth,
-        meta: {
-            middleware: [Middleware.guest]
-        }
-    },
-    {
-        path: '/reset-password',
-        name: 'ResetPassword',
-        component: ResetPassword,
         meta: {
             middleware: [Middleware.guest]
         }
